49f904fb025eda6bc50801880f00a4ea6594c30e
esmoley <esmoley@gmail.com>
2018-02-20 17:39:04 +0300

    1

--- routes/web.php
+++ routes/web.php
@@ -10,20 +10,34 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
 });
+*/
+Route::group(array('domain'=>config('app.domain')),function () {
+    Route::get('/admin',function(){
+        return redirect("//admin.".config('app.domain')."/");
+    });
+    Route::get('/',function(){
+        echo "Are you looking for: <a href='http://admin.".config('app.domain')."/'>http://admin.".config('app.domain')."/</a>?";
+    });
+});
+Route::group(array('domain'=>"admin.".config('app.domain')),function () {
+//Route::group(['namespace' => 'Admin', 'prefix' => 'admin'], function () {
+    Route::get('/','Admin\AdminController@index')->name('home');
+    /*Route::get('/', function () {
+        echo "asd";
+    });*/
+    Route::get('/home', 'Admin\AdminController@index')->name('home');
+    Route::get('/dashboard', 'Admin\AdminController@index')->name('home');
 
-Route::get('/','PagesController@index');
-
-Route::resource('/computers','ComputersController');
-
-Route::resource('/users','UsersClubController');
-
-Auth::routes();
-Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/dashboard', 'HomeController@index')->name('home');
+    Route::resource('/computers','Admin\ComputersController');
+    Route::resource('/users','Admin\UsersClubController');
+    //Route::get('/', 'Auth\LoginController@showLoginForm');
+    //Route::post('login', 'Auth\LoginController@login');
+    //Route::post('logout', 'Auth\LoginController@logout');
+    Route::Auth();
+});
 Route::get('/connect',function(){
     return view('_con.get');
 });
--- storage/framework/views/45ed7666e24fe92f22c78ed87bef844f1a4ef950.php
+++ storage/framework/views/45ed7666e24fe92f22c78ed87bef844f1a4ef950.php
@@ -23,22 +23,22 @@
                     &nbsp;
                 </ul>
                 <ul class="nav navbar-nav">
-                        <li class="<?php echo e((Request::is('admin') ? 'active' : '')); ?>">
-                            <a href="<?php echo e(url('admin')); ?>"><i class="fa fa-home"></i> Home</a>
+                        <li class="<?php echo e((Request::is('/') ? 'active' : '')); ?>">
+                            <a href="<?php echo e(url('/')); ?>"><i class="fa fa-home"></i> Home</a>
                         </li>
-                        <li class="<?php echo e((Request::is('admin/computers') ? 'active' : '')); ?>">
-                            <a href="<?php echo e(url('admin/computers')); ?>">Computers</a>
+                        <li class="<?php echo e((Request::is('computers') ? 'active' : '')); ?>">
+                            <a href="<?php echo e(url('computers')); ?>">Computers</a>
                         </li>
-                        <li class="<?php echo e((Request::is('admin/users') ? 'active' : '')); ?>">
-                            <a href="<?php echo e(url('admin/users')); ?>">Users</a>
+                        <li class="<?php echo e((Request::is('users') ? 'active' : '')); ?>">
+                            <a href="<?php echo e(url('users')); ?>">Users</a>
                         </li>
                     </ul>
                 <!-- Right Side Of Navbar -->
                 <ul class="nav navbar-nav navbar-right">
                     <!-- Authentication Links -->
                     <?php if(Auth::guest()): ?>
-                        <li><a href="<?php echo e(route('admin.login')); ?>">Login</a></li>
-                        <li><a href="<?php echo e(route('admin/register')); ?>">Register</a></li>
+                        <li><a href="<?php echo e(route('login')); ?>">Login</a></li>
+                        <li><a href="<?php echo e(route('register')); ?>">Register</a></li>
                     <?php else: ?>
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
