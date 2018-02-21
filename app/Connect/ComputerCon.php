d135c7c4b4a2619e0149cd003ae779e149e56c07
esmoley <esmoley@gmail.com>
2018-02-21 19:30:39 +0300

    regen

--- app/Connect/ComputerCon.php
+++ app/Connect/ComputerCon.php
@@ -10,6 +10,7 @@ class ComputerCon extends Model
     function __construct($request,$respond,$center_con){
         if(!$this->mac($request))return $respond->error("wrong mac");
         if(!$this->computer = $this->getComputer($request,$center_con->id))return $respond->error("computer error");
+        echo "asd";
     }
     private function mac($request){
         if(!$request->mac || strlen($request->mac)<6){
