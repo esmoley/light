05659a3c437948a095faebca5a77e9a3b3578e00
esmoley <esmoley@gmail.com>
2018-02-21 15:08:15 +0300

    regen

--- routes/web.php
+++ routes/web.php
@@ -14,25 +14,21 @@
     return view('welcome');
 });
 */
-Route::group(['namespace' => 'Admin', 'prefix' => 'admin', 'middleware' => 'admin'],function () {
-    Route::get('/','AdminController@index')->name('home');
-    Route::get('/admin', function () {
-        echo "asd";
-    });
-    Route::get('/home', 'AdminController@index')->name('home');
-    Route::get('/dashboard', 'AdminController@index')->name('home');
+Route::get('/admin','Admin\AdminController@index')->name('home');
+//Route::get('/admin', function () {
+    echo "asd";
+//});
+Route::get('/admin/home', 'Admin\AdminController@index')->name('home');
+Route::get('/admin/dashboard', 'Admin\AdminController@index')->name('home');
 
-    Route::resource('/computers','ComputersController');
-    Route::resource('/users','UsersClubController');
-    //Route::get('/', 'Auth\LoginController@showLoginForm');
-    //Route::post('login', 'Auth\LoginController@login');
-    //Route::post('logout', 'Auth\LoginController@logout');
-    Route::Auth();
-});
+Route::resource('/admin/computers','Admin\ComputersController');
+Route::resource('/admin/users','Admin\UsersClubController');
+//Route::get('/', 'Auth\LoginController@showLoginForm');
+//Route::post('login', 'Auth\LoginController@login');
+//Route::post('logout', 'Auth\LoginController@logout');
+Route::Auth();
     
-Route::namespace('Connect')->group(function () {
-    Route::get('/',function(){
-        return view('connect.get');
-    });
-    Route::post('/', 'connect\MainController@store');
+Route::get('/connect',function(){
+    return view('connect.get');
 });
+Route::post('/connect', 'connect\MainController@store');
