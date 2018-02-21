7586311c53f967329a6b9a76906df5a9cee48d04
esmoley <esmoley@gmail.com>
2018-02-21 20:46:53 +0300

    regen

--- app/Enums/ErrorCode.php
+++ app/Enums/ErrorCode.php
@@ -4,11 +4,11 @@ namespace App\Enums;
 
 use Illuminate\Database\Eloquent\Model;
 
-class ErrorCode extends SplEnum
+class ErrorCode extends Enum
 {
     //ComputerCon
     const WRONG_MAC = 100;
     const COMPUTER_ERROR = 101;
     const ANOTHER_PC_LOGGED_IN = 102;
-    
+
 }
