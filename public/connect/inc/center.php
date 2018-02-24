2c28100cd508dd5bbf5c1c3358dc7e191d23b93e
esmoley <esmoley@gmail.com>
2018-02-24 17:24:35 +0300

    origin

--- public/connect/inc/center.php
+++ public/connect/inc/center.php
@@ -8,8 +8,7 @@
             if($this->info=="")$this->respond->error("WRONG_CLUB_PAIR");
         }
         function get_center(){
-            return $this->db->get_one("select *
-			from `center`
+            return $this->db->get_one("select * from `center`
             where username_connect='".mysql_escape_string($_REQUEST['username_connect'])."'
             and username_password='".mysql_escape_string($_REQUEST['username_password'])."'");
         }
